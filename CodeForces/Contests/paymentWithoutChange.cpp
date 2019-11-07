05e43eb0c14f33cee6e6abf79862d52ab015cbae
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-11-06 23:28:17 -0600

    last update

---
+++ CodeForces/Contests/paymentWithoutChange.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+string isPossible(int,int,int,int);
+int main(){
+
+    int a, b ,value, total, testCases;
+    cin >> testCases;
+    return 0;
+}
+
+string isPossible(int a, int b, int value, int total){
+
+}
